56aa40030932964f31f18cb5cbb7a9352302e430
Ally Weir <aweir@twig-world.com>
2019-07-31 18:44:17 +0100

    Add endpoints for each test case
    There are many ways to call `.query()` on pg. Each of these endpoints
    covers a test case that can be manually tested and verified against the
    trace data propagated to Honeycomb.

--- src/exampleRouter.ts
+++ src/exampleRouter.ts
@@ -3,19 +3,32 @@ import { getEnv } from './utils/getEnv';
 
 const router = express.Router();
 
-router.route('/')
-    .get(async (req, res) => {
-        try {
+router.route('/').get(async (req, res) => {
+    try {
             // A query that serves as an example which has query_args.
-            await req.app.locals.pg.query(
+        await req.app.locals.pg.query(
                 'INSERT INTO example_table(foo, bar) VALUES ($1, $2) RETURNING foo',
                 ['hello', 'world'],
             );
-        } catch (err) {
-            console.log('DISASTER!!');
-        }
-        res.status(200).send({
-            status: 'ok',
-        });
+    } catch (err) {
+        console.log('DISASTER!!');
+    }
+    res.status(200).send({
+        status: 'ok',
     });
+});
+
+router.get('/callback/text-only');
+router.get('/callback/with-values');
+router.get('/callback/with-config-obj');
+router.get('/callback/prepared-stmt');
+router.get('/promises/text-only');
+router.get('/promises/with-values');
+router.get('/promises/with-config-obj');
+router.get('/promises/prepared-stmt');
+router.get('/async-await/text-only');
+router.get('/async-await/with-values');
+router.get('/async-await/with-config-obj');
+router.get('/async-await/prepared-stmt');
+
 export default router;
